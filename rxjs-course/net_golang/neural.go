8f8f2767d1992e905d8f519b2508c54f6b0da701
nvitas <vsnikonov@kassa24.kz>
2019-10-22 17:49:37 +0600

    Нейронка на голанг

---
+++ rxjs-course/net_golang/neural.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"api-herobrine-go/helpers"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/NOX73/go-neural"
+	"github.com/NOX73/go-neural/learn"
+	"github.com/NOX73/go-neural/persist"
+)
+
+type NNContext struct {
+	neural *neural.Network
+}
+
+func main() {
+	app := &NNContext{neural: loadNetwork()}
+	go app.LearnNetwork()
+	app.testNetwork()
+	http.HandleFunc("/", app.Default)
+	log.Println("Go!")
+	http.ListenAndServe(":3000", nil)
+
+}
+
+const (
+	mozgFile   = "mozgFile.json"
+	speed      = 0.1
+	tranerFile = "tranerFile.json"
+)
+
+type Sample struct {
+	In  []float64 `json:"in"`
+	Out []float64 `json:"out"`
+}
+type Total struct {
+	Learn []float64
+}
+
+func (t *Total) Add(l float64) {
+	t.Learn = append(t.Learn, l)
+}
+func (t *Total) Replace(l float64) {
+	t.Learn = []float64{l}
+}
+func (t *Total) GetTotal() float64 {
+	r := float64(0)
+	for _, v := range t.Learn {
+		r += v
+	}
+	return r / float64(len(t.Learn))
+}
+
+func AddSample(info Sample) Sample {
+	d := loadSample()
+	d = append(d, info)
+	j, _ := json.Marshal(d)
+	err := ioutil.WriteFile(tranerFile, j, 0644)
+	if err != nil {
+		panic(err)
+	}
+	return info
+}
+
+func (s *NNContext) Calculate(weidth Sample) Sample {
+	weidth.Out = s.neural.Calculate(weidth.In)
+	return weidth
+}
+func Recommendation(res []float64) int {
+	for k, v := range res {
+		if v > 0.8 {
+			return k
+		}
+	}
+	return 0
+}
+func (n *NNContext) testNetwork() {
+	// i := 5
+	log.Println("--------------------------------------")
+	samples := loadSample()
+	c := 0
+	for _, s := range samples {
+		res := n.neural.Calculate(s.In)
+		finish := false
+		for k, v := range res {
+			if v+s.Out[k] > 1.99 {
+				finish = true
+			}
+		}
+		if !finish {
+			c++
+		}
+		// log.Printf("Пойдем бухать => %s, а ожидаем %s \t %.3f \t\t %v",total(res[0]),total(s.Out[0]),e, res)
+	}
+	helpers.Info("%-10s | %d", "неверных ответов", c)
+}
+func (n *NNContext) LearnNetwork() {
+	samples := loadSample()
+	total := Total{}
+	total.Add(learn.Evaluation(n.neural, samples[0].In, samples[0].Out))
+	helpers.Info("%-10s", "Начало обучения")
+	for total.GetTotal() > 0.01 {
+		for _, s := range samples {
+			if s.IsOut() && s.IsIn() {
+				learn.Learn(n.neural, s.In, s.Out, speed)
+				total.Add(learn.Evaluation(n.neural, s.In, s.Out))
+			} else {
+				helpers.Info("Внимание пустые данные")
+			}
+		}
+		// n.saveNetwork()
+		helpers.Info("%-5s | %f", "Уровень ошибки", total.GetTotal())
+	}
+	n.saveNetwork()
+	helpers.Info("%-10s | %.2f", "Результат обучения", total.GetTotal())
+}
+func (n *NNContext) LearnOne(s Sample) {
+	learn.Learn(n.neural, s.In, s.Out, speed)
+	n.saveNetwork()
+}
+
+func loadSample() []Sample {
+	s := []Sample{}
+	b, _ := ioutil.ReadFile(tranerFile)
+	json.Unmarshal([]byte(b), &s)
+	return s
+}
+
+func loadNetwork() *neural.Network {
+	if !fileExists(mozgFile) {
+		if fileCreate(mozgFile) {
+			createNetwork()
+		} else {
+			return nil
+		}
+	}
+	return persist.FromFile(mozgFile)
+}
+
+func (s *NNContext) saveNetwork() {
+	persist.ToFile(mozgFile, s.neural)
+}
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+func fileCreate(filename string) bool {
+	f, err := os.Create(filename)
+	if err != nil {
+		helpers.Info("fileCreate %v", err)
+		return false
+	}
+	defer f.Close()
+	return true
+}
+
+/*
+ * Нейросеть классификации приоритета терминалов
+ * 15 входящих нейронов 4 исходящих нейронов
+ */
+func createNetwork() {
+	n := neural.NewNetwork(625, []int{625, 650, 350, 10})
+	n.RandomizeSynapses()
+	persist.ToFile(mozgFile, n)
+}
+
+func (s *NNContext) Default(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w)
+	result := Welcom{
+		"Neural Network Api",
+		1,
+	}
+	sample := Sample{[]float64{}, []float64{}}
+	switch r.Method {
+	case "GET":
+		Write(result, w)
+		break
+	case "OPTIONS":
+		Write(result, w)
+		break
+	case "POST":
+		// Decode the JSON in the body and overwrite 'tom' with it
+		d := json.NewDecoder(r.Body)
+		err := d.Decode(&sample)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		if sample.Valid() {
+			if sample.IsOut() {
+				for k, v := range sample.Out {
+					if v > 0 {
+						helpers.Info("Учим %v", k)
+					}
+				}
+				s.LearnOne(sample)
+			}
+			as := s.Calculate(sample)
+			Write(as, w)
+			break
+		}
+		BadRequest("Данные не полные", w)
+		break
+	default:
+		NotAllowed(w)
+	}
+}
+
+func setupResponse(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).Header().Set("Accept", "application/json")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+}
+
+func Write(v interface{}, w http.ResponseWriter) {
+	j, _ := json.Marshal(v)
+	w.Write(j)
+}
+func NotAllowed(w http.ResponseWriter) {
+	v := make(map[string]string)
+	v["error"] = "Метод не допустим"
+	j, _ := json.Marshal(v)
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write(j)
+}
+
+func ServerError(err string, w http.ResponseWriter) {
+	v := make(map[string]string)
+	v["error"] = err
+	j, _ := json.Marshal(v)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(j)
+}
+func BadRequest(e string, w http.ResponseWriter) {
+	v := make(map[string]string)
+	v["error"] = e
+	j, _ := json.Marshal(v)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(j)
+}
+func NotFound(w http.ResponseWriter) {
+	v := make(map[string]string)
+	v["error"] = "Данных не обнаружено"
+	j, _ := json.Marshal(v)
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(j)
+}
+
+type Welcom struct {
+	Name    string `json:"name"`
+	Version int64  `json:"version"`
+}
+
+func (s *Sample) Valid() bool {
+	return len(s.In) == 625 && len(s.Out) == 10
+}
+func (s *Sample) IsOut() bool {
+	for _, v := range s.Out {
+		if v > 0 {
+			return true
+		}
+	}
+	return false
+}
+func (s *Sample) IsIn() bool {
+	for _, v := range s.In {
+		if v > 0 {
+			return true
+		}
+	}
+	return false
+}
--- rxjs-course/src/neural.js
+++ rxjs-course/src/neural.js
@@ -1,6 +1,6 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subject, interval } from 'rxjs';
 import { createInput, customContext, rgbToHex } from './canvas';
-import { map, pairwise, switchMap, takeUntil, withLatestFrom, startWith } from 'rxjs/operators';
+import { map, take, scan, pairwise, switchMap, takeUntil, withLatestFrom, startWith } from 'rxjs/operators';
 import { Layer, Network, Architect } from 'synaptic';
 import { saveAs } from 'file-saver';
 
@@ -8,15 +8,18 @@ const canvas = document.querySelector('#canvas');
 const neural = document.querySelector('#neural');
 const double = document.querySelector('#double');
 const clear = document.querySelector('#clear');
+const code = document.querySelector('#code');
 const names = ['ziro_','one_','two_','tree_','four_','five_','six_','seven_','eight_','nine_'];
 
 const ctxn = customContext(neural);
 const ctxc = customContext(canvas);
 const cli = fromEvent(double, 'click');
 const cliclear = fromEvent(clear, 'click');
-
 const stream = cli.pipe(
     map(e => ctxc.getImageData(0, 0, 200, 200).data)
+);
+const addNeural = new Subject().pipe(
+    scan((acc,v) => acc.concat(v), [])
 );
 const imgData = (ctx) => {
     const d = ctx.getImageData(0, 0, 25, 25).data;
@@ -37,7 +40,7 @@ const imgData = (ctx) => {
     });
     return px;
 }
-const getTrainerData = new Promise((resolve) => {
+const getTrainerData = () => {
     const data = [];
     for (let i=0; i<10; i++){
         for (let j=0; j<11; j++){
@@ -50,99 +53,103 @@ const getTrainerData = new Promise((resolve) => {
                     out: [0,0,0,0,0,0,0,0,0,0],
                 }
                 item.out[i] = 1;
-                data.push(item)
+                data.push(item);
+                addNeural.next(item);
                 ctxn.clearRect(0,0,25,25);
-                if(i === 9 && j === 10) {
-                    resolve(data);
-                }
             }
         }
     }
-})
-// create the network
-const createNeuralNetwork = () => {
-    // const inputLayer = new Layer(625);
-    // const h1 = new Layer(350);
-    // const h2 = new Layer(100);
-    // const h3 = new Layer(50);
-    // const outputLayer = new Layer(10);
-    
-    // inputLayer.project(h1);
-    // h1.project(h2);
-    // h2.project(h3);
-    // h3.project(outputLayer);
-    // // const myNetwork = Architect.Perceptron(625,350,100,50,10);
-    // const myNetwork = new Network({
-    //     input: inputLayer,
-    //     hidden: [h1, h2, h3],
-    //     output: outputLayer
-    // });
-
-    getTrainerData.then(db => {
-        // const learningRate = .3;
-        // for (let i = 0; i < 20000; i++){
-        const data = [];
-        db.forEach(item => {
-            if(item.in.length === 625 && item.out.length === 10) {
-                data.push(item);
-                // myNetwork.activate(item.in);
-                // myNetwork.propagate(learningRate, item.out);
-            }
-        })
-        // var fs = require("file-system");
-        // fs.writeFile("datatrainer.json", JSON.stringify(data), (err) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     };
-        //     console.log("File has been created");
-        // });
-        // }
-        // console.log('Сеть готова');
-        // console.log('myNetwork activate', myNetwork.activate(db[0].in));
-        // console.log('myNetwork result', db[0].out);
-    })
-    // return myNetwork;
 }
-// const myPerceptron = createNeuralNetwork();
+
+const setNumber = index => {
+    const img = new Image();
+    img.src = `src/img/${names[index]}0.png`;
+    img.onload = () => {
+        ctxn.clearRect(0, 0, 200, 200);
+        ctxn.drawImage(img, 0, 0, 200, 200);
+    }
+}
+
+const queryNeuralNetwork = () => {
+        const group1 = () => {
+            const a = document.querySelector('input[name=group1]:checked').value;
+            return [0,0,0,0,0,0,0,0,0,0].map((i,k)=>{
+                if(k == a){
+                    return 1;
+                }
+                return i;
+            });
+        }
+        const item = {
+            in: imgData(ctxn),
+            out: group1(),
+        }
+        let m = 0;
+        item.in.forEach(l => m += l)
+        if(m == 0){
+            console.log('Cancel sum', m);
+            return;
+        }
+        code.innerHTML = JSON.stringify(item);
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", res => {
+            if (res.target.readyState === 4) {
+                const r = JSON.parse(res.target.responseText)
+                let b = {num: 0, index: 0};
+                r.out.forEach((item, index) => {
+                    console.log('select', b.num, item, index);
+                    if(b.num < item){
+                        b.num = item
+                        b.index = index;
+                    }
+                })
+                console.log('out', r.out);
+                setNumber(b.index);
+            }
+        });
+        xhr.open("POST", "http://127.0.0.1:3000/");
+        xhr.setRequestHeader("Content-Type", "application/json");    
+        xhr.send(JSON.stringify(item));
+    // }
+}
+// create the network
+const createNeuralNetwork = (data) => {      
+    var xhr = new XMLHttpRequest();
+    // xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", () => {
+    if (this.readyState === 4) {
+        console.log(this.responseText);
+    }
+    });
+    
+    xhr.open("POST", "http://127.0.0.1:3000/");
+    xhr.setRequestHeader("Content-Type", "application/json");    
+    xhr.send(JSON.stringify(data));
+}
 let s = 0;
-stream.subscribe(pos => {
+stream.subscribe(() => {
     // const p = ctx.getImageData(pos.x, pos.y, 1, 1).data;
     // const hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-    
+    const a = document.querySelector('input[name=group1]:checked').value;
     const img = new Image();
-    img.src = `src/img/${names[s]}9.png`;
+    img.src = `src/img/${names[a]}0.png`;
     img.onload = () => {
         ctxn.clearRect( 0, 0, 200, 200);
-        ctxn.drawImage(img, 0, 0, 200, 200);
-        const d = ctxn.getImageData(0, 0, 200, 200).data;
-        let sum = 0;
-        const px = [];
-        d.forEach((element, index) => {
-            if((index % 4) === 3 && sum > 0){ 
-                px.push(1);
-                sum = 0;
-                return;
-            }
-            if((index % 4) === 3 && sum === 0){
-                px.push(0);
-                sum = 0;
-                return;
-            }
-            sum += element;
-        });
-        // console.log('drawImage', px); 
+        ctxn.drawImage(img, 0, 0, 25, 25);
+        queryNeuralNetwork();
     };
-    s++;
-    if(s === 10) { s = 0;}
-    // .then(db => console.log('drawImage', getTrainerData));
-
-    // pos.forEach((element, index) => {
-    //     if(element > 0)
-    //     console.log('drawImage', index, element); 
-    // });
 })
 cliclear.subscribe(() => {
     ctxc.clearRect(0,0,200,200);
     ctxn.clearRect(0,0,200,200);
+    // getTrainerData(addNeural.next.bind(this))
+})
+addNeural.subscribe(item => {
+    if (item.length >= 110) {
+        interval(1000).pipe(
+            take(item.length),
+            map(v => item[v]),
+        ).subscribe(createNeuralNetwork)
+    }
 })
